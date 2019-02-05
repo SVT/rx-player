d079d1344c2b9b627deb326b1f647005ad6a0f7f
Matteus Hemström <matteus.hemstrom@valtech.se>
2019-02-05 14:02:37 +0100

    Setup failing test for inital time bug

--- tests/integration/scenarios/dash_live_no_timeline.js
+++ tests/integration/scenarios/dash_live_no_timeline.js
@@ -12,13 +12,27 @@ import {
   URLs
 } from "../../contents/DASH_dynamic_SegmentTemplate";
 
+const debugit = function (...args) {
+  describe('debug', function() {
+    before(function() {
+      RxPlayer.LogLevel = "DEBUG";
+    });
+    after(function() {
+      RxPlayer.LogLevel = "NONE";
+    });
+
+    it(...args);
+  });
+};
+
 // describe("DASH dynamic content (SegmentTemplate)", function () {
 //   launchTestsForContent(URLs, manifestInfos);
 // });
 
 // RxPlayer.LogLevel = "DEBUG";
 
-const NOW = 1549292427758;
+const TIME_JUST_WHEN_AVAILABLE = 1549292412000;
+const TIME_NOW = 1549292427758;
 
 describe("DASH live content (SegmentTemplate)", function () {
   let player;
@@ -30,7 +44,7 @@ describe("DASH live content (SegmentTemplate)", function () {
     fakeServer = sinon.fakeServer.create();
     fakeServer.autoRespond = true;
     fakeClock = sinon.useFakeTimers({
-      now: NOW,
+      now: TIME_NOW,
       shouldAdvanceTime: true,
     });
 
@@ -83,6 +97,21 @@ describe("DASH live content (SegmentTemplate)", function () {
     expect(player.getPosition()).to.be.below(0.25);
     expect(player.getVideoLoadedTime()).to.be.above(0);
     expect(player.getVideoPlayedTime()).to.be.above(0);
+  });
+
+  debugit('should stay in loaded state if period is not within initial time', async function () {
+    fakeClock.setSystemTime(TIME_JUST_WHEN_AVAILABLE);
+
+    player.loadVideo({
+      transport: manifestInfos.transport,
+      url: manifestInfos.url,
+    });
+
+    await sleep(200);
+
+    expect(player.getManifest()).not.to.equal(null);
+
+    await waitForState(player, "LOADING");
   });
 });
 
